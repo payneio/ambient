5f3cff303dd7c06b9d0f88856039f5574e582d72
Paul Payne <paul@payne.io>
2018-07-15 14:40:05 -0700

    Roughs out reflex component

--- reflex/reflex.go
+++ reflex/reflex.go
@@ -1,1 +1,10 @@
 package reflex
+
+// TODO: Support the following kinds of rules:
+// - anyone is in the workshop => tv:on, overhead light: on, workbench light: on and workstations:awake
+// - nobody is in the workship => tv:off, overhead light: off, workbench light: off and workstations:asleep
+
+// maintain immutable state trees: actual, desired
+// when actual state matches X, issue command
+// commands transform actual state to a new desired state
+// ambientd then takes care of achieving desired state
