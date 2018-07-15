4631cc085dd41647ec3ea9c6d58082bd2c8e6e14
Paul Payne <paul@payne.io>
2018-07-15 14:41:34 -0700

    Adds registry to discovery component

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -2,10 +2,11 @@ package discovery
 
 import (
 	"github.com/payneio/ambient"
+	"github.com/payneio/ambient/registry"
 	"github.com/payneio/ambient/systems/smartthings"
 )
 
-func Discover(config ambient.Config) {
+func Discover(config ambient.Config, registry *registry.Registry) {
 
 	smartthings := smartthings.System{}
 	smartthings.Authenticate(ambient.Credentials{
